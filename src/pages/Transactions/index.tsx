7d801c5748b9a291d5abccc525b477ce086adaab
CiceroRMG <cicero.ufsc@gmail.com>
2024-12-05 16:43:17 -0300

    feat: adiciona funcionalidades de formatação da data e dos valores

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -7,6 +7,7 @@ import {
   TransactionsTable,
 } from './styles'
 import { useTransactionsContext } from '../../contexts/TransactionsContext'
+import { dateFormatter, priceFormatter } from '../../utils/formatter'
 
 export function Transactions() {
   const { transactions } = useTransactionsContext()
@@ -27,11 +28,14 @@ export function Transactions() {
                   <td width="50%">{transaction.description}</td>
                   <td>
                     <PriceHighlight variant={transaction.type}>
-                      R$ {transaction.price}
+                      {transaction.type === 'outcome' && '- '}
+                      {priceFormatter.format(transaction.price)}
                     </PriceHighlight>
                   </td>
                   <td>{transaction.category}</td>
-                  <td>{transaction.createdAt}</td>
+                  <td>
+                    {dateFormatter.format(new Date(transaction.createdAt))}
+                  </td>
                 </tr>
               )
             })}
